f3afd0826e7a37b9f219dcde755a7bf9d63563b6
Pedro Bruneli <smurfdebruneli@hotmail.com>
2023-09-18 16:29:55 -0300

    feat: implement kafka to payments repo

--- payments/src/main.ts
+++ payments/src/main.ts
@@ -1,10 +1,23 @@
 import { NestFactory } from '@nestjs/core';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import { AppModule } from './app.module';
 
 const port = process.env.PORT || 3002;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.KAFKA,
+    options: {
+      client: {
+        brokers: ['localhost:29092'],
+      },
+      consumer: {
+        groupId: 'orders-consumer',
+      },
+    },
+  });
+  await app.startAllMicroservices();
   await app.listen(port);
 }
 bootstrap();
